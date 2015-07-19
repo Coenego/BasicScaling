5276f114b1a095c02e2f43f6ff66b83108d5a1ed
Mathieu Decoene <hello@mathieudecoene.be>
2015-07-19 10:28:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# BasicScaling
-Basic example of horizontal scaling
+# Basic Scaling
+This is a basic example of how to horizontally scale an application using Nginx.
+
+### Prerequisites
+- Nginx
+- Node.js
+
+### Installation
+1. Clone the repository
+2. Execute `npm install -d`
+3. Add `scaling.example.local` to your hosts file
+4. Execute `nginx -c /path/to/BasicScaling/nginx/nginx.conf`
+5. Execute `node app.js 2001`, `node app.js 2002` and `node app.js 2003`
---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+/**
+ * Create a new Express application
+ *
+ * @private
+ */
+var createServer = function() {
+
+    // Get the port from the command line
+    var port = parseInt(process.argv[2]) || 2000;
+
+    // Create a new Express application
+    var app = express();
+
+    // Create a REST endpoint
+    app.get('/', function(req, res) {
+        res.status(200).send('Served by http://127.0.0.1:' + port);
+    });
+
+    // Start listening for connections
+    var server = app.listen(port, function() {
+        console.log('Example app listening at http://127.0.0.1:%s', port);
+    });
+};
+
+createServer();
